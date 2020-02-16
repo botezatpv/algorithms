3dcfe83b02c8d6c1624aa05f3ad57ad3756a6356
Pavel Botezat <pavel.botezat@it.abb.com>
2020-02-16 11:57:49 +0300

    #1 implemented insertion sort algorithm

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,1 +1,19 @@
-console.log('Hello world!');
+const insertionSort = (array) => {
+  var t0 = performance.now();
+  for(let i = 1; i < array.length; i++) {
+    insert(array, i, array[i]);
+  }
+  var t1 = performance.now();
+  console.log(`Array of n = ${array.length} elements: time ${t1 - t0}! ${array}`);
+}
+
+const insert = (array, position, value) => {
+  let i = position - 1;
+  while (i >= 0 && array[i] > value) {
+    array[i + 1] = array[i];
+    i--;
+  }
+  array[i + 1] = value;
+}
+insertionSort([8, 5, 10, 9]);
+insertionSort([10, 9, 8, 5, 10, 9, 8, 5]);
