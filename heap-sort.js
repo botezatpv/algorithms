838b9dd8c6c3e6ffb46c75ce6a6ccb95aa91ec76
Pavel Botezat <botezatpv@gmail.com>
2020-02-18 20:09:14 +0300

    #3 started to build proper pyramid

--- heap-sort.js
+++ heap-sort.js
@@ -1,8 +1,14 @@
 const heapSort = (array) => {
   var t0 = performance.now();
+  for (let i = 1; i < array.length; i = (i * 2) + 1) {
+    heapify(array, i, i * 2);
+  }
   var t1 = performance.now();
   algorithmTimes['heapSort'] = {...algorithmTimes['heapSort'], [array.length]: t1 - t0};
 
+};
+
+const heapify = (array, startPoint, endPoint) => {
 };
 
 
