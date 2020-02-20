f5a3840a2f22c9c24d20b4a6d6bb597d2c612feb
Pavel Botezat <botezatpv@gmail.com>
2020-02-20 15:44:07 +0300

    #3 built correct tree (Before sorting)

--- heap-sort.js
+++ heap-sort.js
@@ -1,6 +1,6 @@
 const heapSort = (array) => {
   var t0 = performance.now();
-  for (let i = 1; i < array.length; i = (i * 2) + 1) {
+  for (let i = 0; i < (array.length) / 2; i++) {
     heapify(array, i, i * 2);
   }
   var t1 = performance.now();
@@ -8,11 +8,16 @@ const heapSort = (array) => {
 
 };
 
-const heapify = (array, startPoint, endPoint) => {
+const heapify = (array, startPoint, depth) => {
+  const largets = array[startPoint];
+  const left = 2*startPoint + 1;
+  const right = 2*startPoint + 2;
+  console.log(`${array[left]}-${array[startPoint]}-${array[right]}`)
+
 };
 
-
-heapSort(arr10);
-heapSort(arr100);
-heapSort(arr1000);
-heapSort(arr10000);
+heapSort([8, 11, 09, 02, 10, 16, 18, 22, 13, 31, 44, 56])
+// heapSort(arr10);
+// heapSort(arr100);
+// heapSort(arr1000);
+// heapSort(arr10000);
