189c51c7228fdad276d0e6903da3b64e138b3432
Pavel Botezat <botezatpv@gmail.com>
2020-02-21 01:21:42 +0300

    #3 performed initial sorting

--- heap-sort.js
+++ heap-sort.js
@@ -5,16 +5,32 @@ const heapSort = (array) => {
   }
   var t1 = performance.now();
   algorithmTimes['heapSort'] = {...algorithmTimes['heapSort'], [array.length]: t1 - t0};
-
+  console.log('final array:', array);
 };
 
-const heapify = (array, startPoint, depth) => {
-  const largets = array[startPoint];
+const heapify = (array, startPoint) => {
+  let largest = startPoint;
   const left = 2*startPoint + 1;
   const right = 2*startPoint + 2;
-  console.log(`${array[left]}-${array[startPoint]}-${array[right]}`)
+  const max = array.length;
+  console.log(`${array[left]}-${array[startPoint]}-${array[right]}`);
+  if (largest < max) {
+    if (array[largest] < array[right]) {
+      largest = right;
+    } else if (array[largest] < array[left]) {
+      largest = left;
+    }
+  
+    if (largest != array[startPoint]) {
+      const startPointValue = array[startPoint];
+      array[startPoint] = array[largest];
+      array[largest] = startPointValue;
+      // heapify(array, largest);
+    }
+  }
 
 };
+
 
 heapSort([8, 11, 09, 02, 10, 16, 18, 22, 13, 31, 44, 56])
 // heapSort(arr10);
