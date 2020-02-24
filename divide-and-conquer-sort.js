af2bcbf873dad863d390e14bbb840bf3696c4edf
Pavel Botezat <botezatpv@gmail.com>
2020-02-24 11:56:12 +0300

    #4 created partition function for array

--- divide-and-conquer-sort.js
+++ divide-and-conquer-sort.js
@@ -1,12 +1,38 @@
-const divideAndConquerSort = (array) => {
+const divideAndConquerSort = (array, left, right, pivotIndex) => {
   var t0 = performance.now();
-  var t1 = performance.now();
+ 
+  console.log(array, store);
 
-  algorithmTimes['divideAndConquerSort'] = {...algorithmTimes['divideAndConquerSort'], [array.length]: t1 - t0};
+  var t1 = performance.now();
+  // algorithmTimes['divideAndConquerSort'] = {...algorithmTimes['divideAndConquerSort'], [array.length]: t1 - t0};
   console.log(`Array of n = ${array.length} elements: time ${t1 - t0}!`);
 }
 
-divideAndConquerSort(arr10);
-divideAndConquerSort(arr100);
-divideAndConquerSort(arr1000);
-divideAndConquerSort(arr10000);
+const partition = (array, left, right, pivotIndex) => {
+  const pivot = array[pivotIndex];
+  swap(array, right, pivotIndex);
+  let store = left;
+
+  for (let i = left; i < right; i++) {
+    if (array[i] < pivot) {
+      swap(array, i, store);
+      store++;
+    }
+  }
+
+  swap(array, right, store);
+  console.log(array);
+  return store
+}
+
+const swap = (array, i, j) => {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+}
+partition([5, 4, 3, 2, 1], 0, 4, 2);
+partition([1, 2, 5, 3, 4], 0, 4, 2);
+// partition(arr10, 0, 5, 9);
+// divideAndConquerSort(arr100);
+// divideAndConquerSort(arr1000);
+// divideAndConquerSort(arr10000);
